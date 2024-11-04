f42a3760a2b58c2f3b1dfa8a604aa721175481e9
Jude-dev46 <judeoscar2002@gmail.com>
2024-11-04 02:05:07 +0100

    refactored app.js

--- test/index.js
+++ test/index.js
@@ -5,12 +5,6 @@ Scheduler.on("scheduleJob", () => {
   Scheduler.scheduleJob("task", 3000, () =>
     console.log("Good afternoon dear Jude!")
   );
-  Scheduler.scheduleJob("meeting", 10000, () =>
-    console.log("Meeting is ongoing!")
-  );
-  Scheduler.scheduleJob("meeting1", 15000, () =>
-    console.log("Meeting ending soon!")
-  );
 });
 
 // Scheduler.emit("scheduleJob");
